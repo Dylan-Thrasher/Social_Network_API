45712f0ec69d026a7e9342cd1ba6b6491069b1fe
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-05-29 02:17:19 -0400

    updated index in api with comments on usage

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 
+// setting requirement
 const userRoute = require('./userRoute');
 const friendsRoute = require('./friendsRoute');
 const reactionRoute = require('./reactionRoute');
 const thoughtsRoute = require('./thoughtsRoute');
 
+// using requirementto define route
 router.use('/users', userRoute);
 router.use('/friends', friendsRoute);
 router.use('/reactions', reactionRoute);
